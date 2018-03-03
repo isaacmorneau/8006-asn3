bcc33936dec2bcd1185e14f70d2298a430fc03d9
John Agapeyev <jagapeyev@gmail.com>
2018-03-03 13:12:38 -0800

    Added check for root

--- src/main.c
+++ src/main.c
@@ -1,12 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     int ret;
 
     //check for shell
     ret = system(0);
-    printf( "Shell is%s avalible\n", ret ? "" : " not");
+    printf("Shell is%s avalible\n", ret ? "" : " not");
 
-    return  0;
+    if (getuid()) {
+        printf("Program not running as root, attempting to gain root privileges\n");
+        if (setuid(0) == -1) {
+            if (errno == EPERM) {
+                printf("Insufficient permissions, exiting...\n");
+                return EXIT_FAILURE;
+            }
+            perror("setuid");
+            return EXIT_FAILURE;
+        }
+        printf("Root access obtained\n");
+    }
+
+    return EXIT_SUCCESS;
 }
