f28add4e55f6e2ce0d7384ea05b8d6e0869349fe
John Agapeyev <jagapeyev@gmail.com>
2018-03-03 13:30:07 -0800

    Fixed blocking, and added unblocking

--- src/main.c
+++ src/main.c
@@ -12,6 +12,53 @@ void block_ip(const char *restrict ip) {
     const char *drop = " -j DROP";
 
     char command[strlen(forward) + strlen(ip) + strlen(drop) + 1];
+    strcat(command, input);
+    strcat(command, ip);
+    strcat(command, drop);
+
+    system(command);
+
+    memset(command, 0, sizeof(command));
+
+    strcat(command, output);
+    strcat(command, ip);
+    strcat(command, drop);
+
+    system(command);
+
+    memset(command, 0, sizeof(command));
+
+    strcat(command, forward);
+    strcat(command, ip);
+    strcat(command, drop);
+
+    system(command);
+}
+
+//ip must be null-terminated, and contain the ip to block as a string
+void unblock_ip(const char *restrict ip) {
+    const char *input   = "iptables -D INPUT ";
+    const char *output  = "iptables -D OUTPUT ";
+    const char *forward = "iptables -D FORWARD ";
+    const char *drop = " -j DROP";
+
+    char command[strlen(forward) + strlen(ip) + strlen(drop) + 1];
+    strcat(command, input);
+    strcat(command, ip);
+    strcat(command, drop);
+
+    system(command);
+
+    memset(command, 0, sizeof(command));
+
+    strcat(command, output);
+    strcat(command, ip);
+    strcat(command, drop);
+
+    system(command);
+
+    memset(command, 0, sizeof(command));
+
     strcat(command, forward);
     strcat(command, ip);
     strcat(command, drop);
